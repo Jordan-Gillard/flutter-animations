cfe9d66d2d6df72f56dec689f18c6a28c5199bf7
Jordan Gillard <jordan-gillard@outlook.com>
2020-07-12 16:59:36 -0500

    feat: Flutter logo animation starts small and grows

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LogoApp());
 }
 
-class MyApp extends StatelessWidget {
+class LogoApp extends StatefulWidget {
+  @override
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {
+          //
+        });
+      });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
